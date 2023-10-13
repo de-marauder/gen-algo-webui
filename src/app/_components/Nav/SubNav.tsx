e51d0ed02e0fc5116b6ad8be8427bfd8c825206d
de-marauder <ezikegodson@gmail.com>
2023-10-13 13:04:10 +0100

    fix: adjusted styles for better UX

--- src/app/_components/Nav/SubNav.tsx
+++ src/app/_components/Nav/SubNav.tsx
@@ -11,10 +11,10 @@ export const SubNav = ({ nav }: {
   const pathName = usePathname();
 
   const activeTabColor = 'green'
-  const classes = 'border border-green-800 h-fit px-4 py-2 cursor-pointer bg-transparent rounded-xl hover:bg-green-600/20'
+  const classes = 'border border-green-800 h-fit px-4 py-2 cursor-pointer bg-transparent rounded-xl hover:bg-green-600/20 max-sm:w-full text-center'
   return (
-    <nav className='bg-blue-800/10 rounded rounded-2xl mt-4 mb-4 mx-auto p-2 flex justify-center items-center'>
-      <ul className='flex justify-center gap-4'>
+    <nav className='bg-blue-800/10 rounded rounded-2xl mt-4 mb-4 mx-auto flex justify-center items-center'>
+      <ul className='flex justify-center gap-4 max-sm:w-full'>
         <>
           {
             nav.map((el, id) => {
